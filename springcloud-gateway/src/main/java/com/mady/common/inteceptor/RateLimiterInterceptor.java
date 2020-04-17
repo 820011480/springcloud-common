d81b1aa8d78bc6ed4d744c1c8a397619f4a2b356
mady@ibuscloud.com <mady@ibuscloud.com>
2020-04-17 17:07:55 +0800

    [fixed] 添加限流

--- springcloud-gateway/src/main/java/com/mady/common/inteceptor/RateLimiterInterceptor.java
+++ springcloud-gateway/src/main/java/com/mady/common/inteceptor/RateLimiterInterceptor.java
@@ -1,10 +1,31 @@
 package com.mady.common.inteceptor;
 
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * @author mady
  * @version 1.0.0
  * @date 2020/4/17 16:22
- * @description
+ * @description 分布式限流拦截器
  */
-public class RateLimiterInterceptor {
+public class RateLimiterInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
 }

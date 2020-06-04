944bbb2b0e0e4fd7afa2ac08dc4580d9ae328f52
mady@ibuscloud.com <mady@ibuscloud.com>
2020-06-04 15:25:43 +0800

    [fixed]增加redis模块

---
+++ springcloud-basics/src/main/java/com/mady/common/design/single/SingletonClient.java
@@ -0,0 +1,14 @@
+package com.mady.common.design.single;
+
+/**
+ * @author mady
+ * @version 1.0.0
+ * @date 2020/6/3 13:18
+ * @description 单例客户端
+ */
+public class SingletonClient {
+
+    public static void main(String[] args) {
+
+    }
+}
